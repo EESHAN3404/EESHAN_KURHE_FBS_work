f94489c9eea6e5525d955414b2af5c6e4c196dfe
EESHAN 03 <eeshankurhe3404@gmail.com>
2025-06-05 12:35:55 +0530

    Add files via upload

---
+++ C PROGRAMMING/CLASS TESTS/TEST 1/TESTQ2.cpp
@@ -0,0 +1,16 @@
+//Write a program to check whether a given year is a leap year.
+
+#include<stdio.h>
+
+int main(){
+	int year;
+	printf("Enter year :");
+	scanf("%d",&year);
+	
+	if(year%4==0){
+		printf("%d is a leap year",year);
+	}
+	else{
+		printf("%d is not a leap year",year);
+	}
+}
