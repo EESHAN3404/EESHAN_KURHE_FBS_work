11d5829c73c4d3afb9517ba0927b14bbf3b8f48e
EESHAN 03 <eeshankurhe3404@gmail.com>
2025-06-19 12:12:38 +0530

    Add files via upload

---
+++ C PROGRAMMING/CLASS TESTS/TEST 3/Q2.c
@@ -0,0 +1,20 @@
+//Print alternate numbers in the given range.
+
+#include<stdio.h>
+
+void main(){
+
+	
+	int s,e;
+	printf("Enter the range to start : ");
+    scanf("%d", &s);
+
+    printf("Enter the range to end : ");
+    scanf("%d", &e);
+
+    printf("Even numbers :- ", s, e);
+    for (int n=s;n<=e;n=n+2) 
+	{
+        printf("%d ",n);
+    }  
+}
