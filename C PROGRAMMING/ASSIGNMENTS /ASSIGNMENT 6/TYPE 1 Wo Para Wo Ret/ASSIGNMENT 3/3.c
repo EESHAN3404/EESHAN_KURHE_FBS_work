a5468d273d552828f5955a6c986ec49200938407
EESHAN 03 <eeshankurhe3404@gmail.com>
2025-08-05 17:34:51 +0530

    Update 3.c

--- C PROGRAMMING/ASSIGNMENTS /ASSIGNMENT 6/TYPE 1 Wo Para Wo Ret/ASSIGNMENT 3/3.c
+++ C PROGRAMMING/ASSIGNMENTS /ASSIGNMENT 6/TYPE 1 Wo Para Wo Ret/ASSIGNMENT 3/3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+void sumRange();
+
+int main() {
+    sumRange();
+    return 0;
+}
+
+void sumRange() {
+    int i = 1;
+    int sum = 0;
+    while (i <= 5) {
+        sum += i;
+        i++;
+    }
+    printf("Sum: %d\n", sum);
+}
 
