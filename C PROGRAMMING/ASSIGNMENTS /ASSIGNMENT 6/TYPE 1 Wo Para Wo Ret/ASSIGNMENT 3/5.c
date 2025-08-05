fab96b61346a854420164ea8e7153ce3d565cc70
EESHAN 03 <eeshankurhe3404@gmail.com>
2025-08-05 17:36:06 +0530

    Update 5.c

--- C PROGRAMMING/ASSIGNMENTS /ASSIGNMENT 6/TYPE 1 Wo Para Wo Ret/ASSIGNMENT 3/5.c
+++ C PROGRAMMING/ASSIGNMENTS /ASSIGNMENT 6/TYPE 1 Wo Para Wo Ret/ASSIGNMENT 3/5.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+void checkArmstrong();
+
+void main() {
+    checkArmstrong();
+    return 0;
+}
+void checkArmstrong() {
+    int n, rem, sum = 0, temp;
+
+    printf("Enter the Number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (n > 0) {
+        rem = n % 10;
+        sum += rem * rem * rem;
+        n = n / 10;
+    }
+
+    if (temp == sum)
+        printf("Number is Armstrong\n");
+    else
+        printf("Number is Not Armstrong\n");
+}
 
